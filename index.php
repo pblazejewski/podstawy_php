16b0d165bf4fc30e5a214a2486e818400ff348b6
Student TEB <student@pracownia.lublin.teb.pl>
2024-01-14 14:11:39 +0100

    Czwarty commit

--- index.php
+++ index.php
@@ -10,53 +10,30 @@
 
 <body>
 <?php
-// $typ = "email";
-
-// $Label_and_Input = 
-// "<label for='eo'>Label and input</label>
-// <input type=$typ id='eo'/>
-// ";
-
-//     if($typ == "email") {
-//         echo $Label_and_Input;
-//     } else {
-//         echo "Nope";
-//     }
-
-    // $on_off = 1;
-    // $date_on_off = 0;
-    // $default = 1;
-
-    // if($date_on_off==0){
-    //     $default=0;
-    // }
-
-    // Parzysta pętla
-    // for($i = 0; $i <= 100; $i+=2) {
-    //     echo $i;
-    // }
 
 
-    // Nieparzysta pętla
-    // for ($i = 0; $i <= 100; $i++) {
-    //     if($i % 2 != 0) {
-    //         echo $i;
-    //     }
-    // }
+    $on_off = 1;
+    $date_on_off = 0;
+    $default = 1;
 
-    // for($i = 1; $i <= 100; $i++) {
-    //     echo "{$i}</br>";
-    // }
+    if($date_on_off==0){
+        $default=0;
+    }
 
-    // echo 
-    // "<br><form action=''>
-    // <input type='text' placeholder='Tekst'name='text'>
-    // <input type='email' placeholder='Email' name='email'>
-    // <input type='date'".($default==1 ? "value='2024-01-14'": ""). ($date_on_off == 1 ? "" : "disabled="). "
-    // ". ($on_off == 1 ? "<button>eeo</button>" : "<button type=reset>reset</button>"). "
-    // </form>";
+ 
+    echo 
+    "<form>
+    <input type='text' placeholder='Imie'name='text'>
+    <div>
+    <input type='email' placeholder='Email' name='email'>
+    <input type='date' name='date'>
+    <input type='radio'>
+    <input type='checkbox'>
+    </div>
+    <button type='submit'>Resetuj</button>
+    <button type='reset'>Wyślij</button>
 
-
+    </form>";
 ?>
 
 </body>
