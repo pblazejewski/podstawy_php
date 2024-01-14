e284ecf411c677fc1115d91595f450fe15399d4b
Student TEB <student@pracownia.lublin.teb.pl>
2024-01-14 13:56:46 +0100

    Trzeci commit

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Podstawy php</title>
 </head>
+<!-- Inkrementacja -->
+
 
 <body>
 <?php
@@ -21,21 +23,38 @@
 //         echo "Nope";
 //     }
 
-    $on_off = 1;
-    $date_on_off = 0;
-    $default = 1;
+    // $on_off = 1;
+    // $date_on_off = 0;
+    // $default = 1;
 
-    if($date_on_off==0){
-        $default=0;
-    }
+    // if($date_on_off==0){
+    //     $default=0;
+    // }
 
-    echo 
-    "<form action=''>
-    <input type='text' placeholder='Tekst'name='text'>
-    <input type='email' placeholder='Email' name='email'>
-    <input type='date'".($default==1 ? "value='2024-01-14'": ""). ($date_on_off == 1 ? "" : "disabled="). "
-    ". ($on_off == 1 ? "<button>eeo</button>" : "<button type=reset>reset</button>"). "
-    </form>";
+    // Parzysta pętla
+    // for($i = 0; $i <= 100; $i+=2) {
+    //     echo $i;
+    // }
+
+
+    // Nieparzysta pętla
+    // for ($i = 0; $i <= 100; $i++) {
+    //     if($i % 2 != 0) {
+    //         echo $i;
+    //     }
+    // }
+
+    // for($i = 1; $i <= 100; $i++) {
+    //     echo "{$i}</br>";
+    // }
+
+    // echo 
+    // "<br><form action=''>
+    // <input type='text' placeholder='Tekst'name='text'>
+    // <input type='email' placeholder='Email' name='email'>
+    // <input type='date'".($default==1 ? "value='2024-01-14'": ""). ($date_on_off == 1 ? "" : "disabled="). "
+    // ". ($on_off == 1 ? "<button>eeo</button>" : "<button type=reset>reset</button>"). "
+    // </form>";
 
 
 ?>
