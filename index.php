cdea8958b0cd174f5e9cc94588c8b04c21b38f79
Student TEB <student@pracownia.lublin.teb.pl>
2024-01-14 14:44:41 +0100

    Piaty commit z css

--- index.php
+++ index.php
@@ -3,36 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Podstawy php</title>
 </head>
 <!-- Inkrementacja -->
 
 
 <body>
-<?php
-
-
-    $on_off = 1;
-    $date_on_off = 0;
-    $default = 1;
-
-    if($date_on_off==0){
-        $default=0;
-    }
-
- 
+<?php 
     echo 
     "<form>
-    <input type='text' placeholder='Imie'name='text'>
-    <div>
-    <input type='email' placeholder='Email' name='email'>
-    <input type='date' name='date'>
-    <input type='radio'>
-    <input type='checkbox'>
+    <input type='text' placeholder='Imie'name='text' class='imie-input'>
+    <div class='ogolny-div'>
+        <div class='pierwszy-div'>
+            <input type='email' placeholder='Email' name='email'>
+             <input type='date' name='date'>
+        </div>
+        <div class='drugi-div'>
+            <div class='email'>
+                <input type='radio' class='radio'><label>Nie mam Email</label>
+            </div>
+            <div class='data'>
+            <input type='checkbox' class='checkbox'><label>Dzisiejsza</label>
+            </div>
+        </div>
     </div>
-    <button type='submit'>Resetuj</button>
-    <button type='reset'>Wyślij</button>
-
+    <div class='trzeci-div'>
+        <button type='submit'>Resetuj</button>
+        <button type='reset'>Wyślij</button>
+    </div>
     </form>";
 ?>
 
